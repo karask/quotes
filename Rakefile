6e844f801f66470ecd16ec1b41614a987c1b4fa2
K. Karasavvas <kkarasavvas@gmail.com>
2013-04-11 16:18:16 +0300

    almost finished with schema...

--- Rakefile
+++ Rakefile
@@ -1,5 +1,8 @@
 # could declare vars so that paths are not hard-coded!
 
+
+task :default => [:init_db]
+
 desc 'Create the database schema: db is ready to be populated'
 task :init_db do
     touch 'db/quotes.db' unless File.exist?('db/quotes.db')
--- scripts/init_db_schema.rb
+++ scripts/init_db_schema.rb
@@ -1,11 +1,33 @@
 # encoding: utf-8
+require 'rubygems'
 require 'sequel'
 
 DB = Sequel.sqlite(ARGV[1])
 
 DB.create_table :quotes do
   primary_key :id
+  varchar :quote
+  
+  # many to one with authors
+  foreign_key :author_id, :authors
+end
+
+DB.create_table :authors do
+  primary_key :id
   varchar :name, :size => 50, :unique => true
 end
 
+DB.create_table :tags do
+  primary_key :id
+  varchar :name, :size => 25, :unique => true
+end
 
+# many to many quotes - tags table 
+DB.create_table :quotes_tags do
+  foreign_key :quote_id, :quotes
+  foreign_key :tag_id, :tags
+end
+
+tags = DB[:tags]
+tags.insert(:name => 'Test')
+tags.insert(:name => 'Test2')
