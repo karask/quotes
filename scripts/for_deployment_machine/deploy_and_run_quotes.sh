1504bfa4f1bcd13265b3805951b8d99dec8afb63
Kostas Karasavvas <kkarasavvas@gmail.com>
2013-05-18 23:49:15 +0300

    corrected .bundle/config creation in deploy_and_run_quotes.sh

--- scripts/for_deployment_machine/deploy_and_run_quotes.sh
+++ scripts/for_deployment_machine/deploy_and_run_quotes.sh
@@ -9,7 +9,7 @@ cd quotes
 
 # add sqlite build configuration for when bundler installs gem
 mkdir .bundle
-echo "--with-opt-include=/home/karask/local/lib/sqlite-autoconf-3071602/  --with-opt-lib=/home/karask/local/lib/sqlite-autoconf-3071602/.libs  --with-cflags='-O3 -DSQLITE_ENABLE_ICU' --with-cppflags=`icu-config --cppflags` --with-ldflags=`icu-config --ldflags`" > .bundle/config
+echo "BUNDLE_BUILD__SQLITE3: --with-opt-include=/home/karask/local/lib/sqlite-autoconf-3071602/  --with-opt-lib=/home/karask/local/lib/sqlite-autoconf-3071602/.libs  --with-cflags='-O3 -DSQLITE_ENABLE_ICU' --with-cppflags=`icu-config --cppflags` --with-ldflags=`icu-config --ldflags`" > .bundle/config
 
 bundle install
 
